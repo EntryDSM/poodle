6e4d714dd32121d8883510b8c1588e900a953554
leewoochan <030219woo@naver.com>
2020-04-03 00:42:10 +0900

    [UPDATE] <LoginModal> set true

--- poodle/src/container/common/ModalContainer/LoginModalContainer/LoginModalContainer.tsx
+++ poodle/src/container/common/ModalContainer/LoginModalContainer/LoginModalContainer.tsx
@@ -5,7 +5,7 @@ function LoginModalContainer() {
     return (
         <LoginModal
             title="로그인"
-            contour={false}
+            contour={true}
             error="이메일 또는 비밀번호가 잘못되었습니다"
             color="#78cede"
         />
