99427c01f1c1ae9bca8388d5b8ef264bdac63d9e
leewoochan <030219woo@naver.com>
2020-04-04 05:11:06 +0900

    [ADD] <Join> images

---
+++ poodle/src/assets/Join/index.ts
@@ -0,0 +1,4 @@
+export { default as check } from './check.png';
+export { default as checked } from './checked.png';
+export { default as rightArrow } from './rightArrow.png';
+export { default as validRightArrow } from './validRightArrow.png'
