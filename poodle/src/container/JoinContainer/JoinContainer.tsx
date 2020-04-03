31e2ee82852b811e005debc0e3d2db405b7c7a47
leewoochan <030219woo@naver.com>
2020-04-04 05:16:16 +0900

    [CREATE] <Join> container

---
+++ poodle/src/container/JoinContainer/JoinContainer.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { HeaderContainer } from '../';
+import Join from '../../components/Join/Join';
+
+function JoinContainer() {
+    return (
+        <>
+            <HeaderContainer/>
+            <Join />
+        </>
+    );
+}
+
+export default JoinContainer;
