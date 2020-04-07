6659e9596a8dfd2f0f68cc4f912ead9ec5c90d78
LEEWOOCHAN <030219woo@naver.com>
2020-04-08 02:39:55 +0900

    [DELETE] <modal> 쓸데 없는 코드 제거

--- poodle/src/components/common/Modal/ModalContent.tsx
+++ poodle/src/components/common/Modal/ModalContent.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useState } from 'react';
+import React from 'react';
 import * as S from '@/styles/common/Modal';
 import { ModalContentProps } from './';
 import { BlueSuccess, BlueCheck, RedError, YellowCheck } from '@/assets/common/Modal';
@@ -11,37 +11,14 @@ const imageList: any = {
 }
 
 function ModalContent({ children, title, contour, error, normal, explain, color, icon }: ModalContentProps) {
-    const [a, b] = React.useState('');
-    const [effect, setEffect] = useState(false);
-    const onClick = useCallback(() => {
-        setEffect(true)
-        setTimeout(() => {
-            console.log('delay 1...', a);
-            setEffect(false)
-            a ? b('') : b('이메일 또는 비밀번호가 잘못되었습니다');
-        }, 1000);
-    }, [a, effect]);
-    const onClick2 = useCallback(() => {
-        onClick();
-        setTimeout(() => {
-            setEffect(true);
-        }, 2000);
-        setTimeout(() => {
-            setEffect(false)
-            b('');
-        }, 3000);
-    }, [a, effect]);
     return (
         <S.ModalContentWrapper>
             <S.Title>{title}</S.Title>
             <S.SubTitle
                 contour={contour}
-                error={a}
+                error={error}
                 color={color}
-                effect={effect}
-                // normal={`${a ? '' : 'fsf'}`}
             >
-                {a && a}
                 {normal && normal}
             </S.SubTitle>
             {icon && <S.IconImage src={imageList[icon]} />}
@@ -50,15 +27,8 @@ function ModalContent({ children, title, contour, error, normal, explain, color,
                 {explain}
             </S.ExplainSentence>}
             {children}
-            {/* <button onClick={onClick2}>바꾸기</button> */}
         </S.ModalContentWrapper>
     );
 }
-
-ModalContent.defaultProps = {
-    contour: false,
-    normal: '',
-    error: '',
-};
 
 export default ModalContent;
