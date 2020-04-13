b80d4bfdc163434218fe5284cce5bb7c2e3e0788
sema0710 <pandati@naver.com>
2020-04-13 15:07:48 +0900

    [FIX] git 실수로 인해 손실된 파일 복구

--- poodle/src/App.tsx
+++ poodle/src/App.tsx
@@ -1,9 +1,28 @@
 import React from 'react';
+import { 
+  BrowserRouter, 
+  Route, 
+  Switch 
+} from 'react-router-dom';
+  import GlobalStyle from './styles/globalStyle';
+  import { Footer } from './components/default/Footer';
+  import { 
+    ConnectIntroduction, 
+    ConnectChoiceType,
+    ConnectInfo, 
+  } from './container';
 
 function App() {
   return (
-    <div className="App">
-    </div>
+    <BrowserRouter>
+      <GlobalStyle/>
+      <Switch>
+          <Route path="/Introduction" render={() => <ConnectIntroduction/>}/>
+          <Route path="/Type" render={() => <ConnectChoiceType/>}/>
+          <Route path="/Info" render={() => <ConnectInfo/>}/>
+      </Switch>
+      <Footer/>
+    </BrowserRouter>
   );
 }
 
