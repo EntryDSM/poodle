b0593095ecb217ce13b527bcf69de65bee78ed02
sema0710 <pandati@naver.com>
2020-04-16 16:37:02 +0900

    [FIX] 간격 수정(급함)

--- poodle/src/styles/Footer/index.ts
+++ poodle/src/styles/Footer/index.ts
@@ -4,20 +4,24 @@ const FooterColor = "#62d3e8";
 const FooterFontColor = "white";
 
 export const FooterDiv = styled.div`
-    width: 100%;
-    height: 204px;
-    background-color: ${FooterColor};
-    color: ${FooterFontColor};
-    padding: 20px 375px;
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    box-sizing: border-box;
     > div {
-        box-sizing: content-box;
+        width: 100%;
+        height: 204px;
+        background-color: ${FooterColor};
+        color: ${FooterFontColor};
+        padding: 20px 0px;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        box-sizing: border-box;
+        > div {
+            box-sizing: content-box;
+        }
+        > div:first-child {
+            margin-right: 41.625rem;
+        }
     }
 `
-
 export const FooterTitle = styled.p`
     font-size: 33px;
     margin-bottom: 8px; 
@@ -32,17 +36,20 @@ export const FooterText = styled.div<{ margin?: string }>`
 export const FooterButton = styled.p`
     font-size: 16px;
     margin: 16px;
+    text-align: right;
     cursor: pointer;
 `
 
 export const FooterImgButtonDiv = styled.div`
     display: flex;
     justify-content: flex-end;
+    padding: 8px;
+    box-sizing: border-box;
 `
 
 export const FooterImgButton = styled.div`
     width: 20px;
     height: 20px;
-    padding: 8px;
+    padding: 8px 8px 8px 0px;
     cursor: pointer;
 `
