30838c058e5b635c867564db6158e325a2d6b20b
leewoochan <030219woo@naver.com>
2020-04-19 04:31:54 +0900

    [ADD] <App.tsx> applystatus and error route 설정

--- poodle/src/App.tsx
+++ poodle/src/App.tsx
@@ -7,6 +7,8 @@ import {
 import { Global } from './styles/common'
 import { 
   Footer,
+  ApplyStatus,
+  Error
 } from './components';
 
 function App() {
@@ -15,7 +17,9 @@ function App() {
       <HeaderContainer />
       <Global/>
       <Switch>
+        <Route path="/applystatus" component={ApplyStatus} />
         <Route path="/join" component={JoinContainer} />
+        <Route component={Error} />
       </Switch>
       <Footer />
     </BrowserRouter>
