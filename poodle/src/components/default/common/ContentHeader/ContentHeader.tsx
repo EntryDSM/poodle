521468687f74dd963416017d4f57f7b9315e448a
leewoochan <030219woo@naver.com>
2020-04-19 04:41:53 +0900

    [CREATE] <common> ContentHeader

---
+++ poodle/src/components/default/common/ContentHeader/ContentHeader.tsx
@@ -0,0 +1,25 @@
+import React, { FC } from 'react';
+import * as S from '@/styles/common/ContentHeader';
+
+type ContentHeaderProps = {
+    padding: string,
+    subTitle?: string,
+    title: string,
+    underLineLength: number,
+    border?: string
+    titleFontSize?: number
+}
+
+const ContentHeader: React.FC<ContentHeaderProps> = ({
+    padding, subTitle, title, underLineLength, border, titleFontSize
+}) => {
+    return (
+        <S.ContentHeaderWrapper padding={padding} border={border}>
+            <S.SubTitle>{subTitle}</S.SubTitle>
+            <S.Title fontSize={titleFontSize}>{title}</S.Title>
+            <S.UnderLine length={underLineLength} />
+        </S.ContentHeaderWrapper>
+    );
+};
+
+export default ContentHeader;
