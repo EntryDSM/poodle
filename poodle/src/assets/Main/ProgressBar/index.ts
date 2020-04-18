e947234c96099509a81e85ad230258a67e3e01ae
leewoochan <030219woo@naver.com>
2020-04-19 04:18:40 +0900

    [ADD] <main_progress_bar> images

---
+++ poodle/src/assets/Main/ProgressBar/index.ts
@@ -0,0 +1,4 @@
+export { default as AllFinish } from './AllFinishIcon.png';
+export { default as Finish } from './FinishIcon.png';
+export { default as Ing } from './IngIcon.png';
+export { default as XIcon } from './XIcon.png';
