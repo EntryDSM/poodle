d96e9e38d6a34b8ac1ecc6e99f2a4fbddcac7e50
sema0710 <pandati@naver.com>
2020-04-30 22:04:59 +0900

    [CREATE] user_type index js
    까먹고 안올렸습니다

---
+++ poodle/src/components/ChoiceType/index.ts
@@ -0,0 +1,19 @@
+import DefaultRow from './RowType/defaultRow';
+import { 
+    QualificationExam, 
+    ChoiceTypeRow, 
+    ChoiceDistrict, 
+    GraduationStatus,
+    GraduationYear,
+    Specialty,
+} from './RowType';
+
+export { 
+    DefaultRow, 
+    QualificationExam, 
+    ChoiceTypeRow, 
+    ChoiceDistrict, 
+    GraduationStatus,
+    GraduationYear,
+    Specialty,
+};
