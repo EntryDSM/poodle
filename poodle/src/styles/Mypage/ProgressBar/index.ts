f7cb4c60d5b5e51c59fabaea9945e4c0b5421208
leewoochan <030219woo@naver.com>
2020-05-06 22:42:10 +0900

    [CREATE] <mypage> css

---
+++ poodle/src/styles/Mypage/ProgressBar/index.ts
@@ -0,0 +1,52 @@
+import styled from 'styled-components';
+import { finishIcon, notFinishIcon } from '@/assets/Mypage';
+import { Link } from 'react-router-dom';
+
+export const ProgressBarWrapper = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    padding-bottom: 70px;
+`;
+
+export const ProgressBox = styled.div`
+    width: 980px;
+    height: 126px;
+    display: flex;
+`;
+
+export const ProgressItemBox = styled.div`
+    width: 91px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+`;
+
+export const ProgressImageLink = styled(Link)<{ isfinished: string }>`
+    text-decoration: none;
+    width: 80px;
+    height: 80px;
+    background-size: contain;
+    margin-bottom: 24px;
+    background-image: url(${({ isfinished }) => isfinished === 'true' ? finishIcon : notFinishIcon});
+`;
+
+export const ProgressTitleLink = styled(Link)`
+    text-decoration: none;
+    width: 100px;
+    height: 22px;
+    font-family: NanumSquareR;
+    font-size: 20px;
+    line-height: 1.05;
+    text-align: center;
+    color: #afafaf;
+    letter-spacing: -2px;
+    `;
+    
+    export const Line = styled.hr`
+    width: 200px;
+    height: 3px;
+    border: none;
+    background-color: #AFAFAF;
+    margin: 40px 10px 0;
+`;
---
+++ poodle/src/styles/Mypage/SubmitDocument/index.ts
@@ -0,0 +1,7 @@
+import styled from 'styled-components';
+import { Wrapper, Container } from '@/styles/common';
+
+export { Wrapper as Wrapper };
+export const DocumentContainer = styled(Container)`
+    padding-bottom: 96px;
+`;
