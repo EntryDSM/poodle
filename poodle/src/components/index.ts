2706a7d189a34d2ad552b52bcb23af9847e56e76
_uchanlee <uchanlee.dev@gmail.com>
2020-05-10 18:47:32 +0900

    Update poodle/src/components/index.ts
    Co-authored-by: Roy Jeong <roy.jeong@sendbird.com>

--- poodle/src/components/index.ts
+++ poodle/src/components/index.ts
@@ -1,5 +1,5 @@
 export { default as ApplyStatus } from './ApplyStatus/ApplyStatus';
-export { Footer as Footer } from './default/Footer';
+export { Footer } from './default/Footer';
 export { default as Error } from './Error/Error';
 export { default as Mypage } from './Mypage/Mypage';
 export { default as Document } from './Mypage/Document/Document';
