e641feea509c789152d698f80f54e87bcab2dc88
leewoochan <030219woo@naver.com>
2020-05-10 19:36:41 +0900

    [DELETE] <Mypage> 쓸데없는 assertion

---
+++ poodle/src/styles/Mypage/Document/index.ts
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+import { Wrapper, Container } from "@/styles/common";
+
+export { Wrapper };
+export const DocumentContainer = styled(Container)`
+  padding-bottom: 96px;
+`;
--- poodle/src/styles/Mypage/SubmitDocument/index.ts
+++
@@ -1,7 +0,0 @@
-import styled from 'styled-components';
-import { Wrapper, Container } from '@/styles/common';
-
-export { Wrapper as Wrapper };
-export const DocumentContainer = styled(Container)`
-    padding-bottom: 96px;
-`;
