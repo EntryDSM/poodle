157edf0db6c5477b420a0c38ecb43267f003ea7c
sema0710 <pandati@naver.com>
2020-05-14 19:36:40 +0900

    [FIX] 경로를 간단하게 수정

--- poodle/src/components/ChoiceType/index.ts
+++ poodle/src/components/ChoiceType/index.ts
@@ -1,19 +1,3 @@
 import DefaultRow from './RowType/defaultRow';
-import { 
-    QualificationExam, 
-    ChoiceTypeRow, 
-    ChoiceDistrict, 
-    GraduationStatus,
-    GraduationYear,
-    Specialty,
-} from './RowType';
 
-export { 
-    DefaultRow, 
-    QualificationExam, 
-    ChoiceTypeRow, 
-    ChoiceDistrict, 
-    GraduationStatus,
-    GraduationYear,
-    Specialty,
-};
+export { DefaultRow }
