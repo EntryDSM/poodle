72a9b6a4f444a47a5b221560d1c240d617a4cca2
ohjunsang <pandati@dsm.hs.kr>
2020-06-03 19:12:51 +0900

    [CREATE] redux 연결 및 rows 합침

---
+++ poodle/src/container/Info/ConnectInfo.ts
@@ -0,0 +1,71 @@
+import { connect } from 'react-redux';
+import Info from './Info';
+import { RootState } from '../../core/redux/reducer';
+import {
+    setAddress,
+    setBirthday,
+    setGender,
+    setName,
+    setNumber,
+    setPicture,
+    setMiddleSchool,
+    setSchoolPhoneNum,
+    setProtectorPhoneNum,
+    setPhoneNum,
+    setProtectorName,
+} from '../../core/redux/actions/Info';
+
+export const mapStateToProps = (state: RootState) => ({
+    address: state.InfoState.address,
+    birthday: state.InfoState.birthday,
+    gender: state.InfoState.gender,
+    name: state.InfoState.name,
+    number: state.InfoState.number,
+    picture: state.InfoState.picture,
+    middleSchool: state.InfoState.middleSchool,
+    schoolPhoneNum: state.InfoState.schoolPhoneNum,
+    protectorPhoneNum: state.InfoState.protectorPhoneNum,
+    phoneNum: state.InfoState.phoneNum,
+    protectorName: state.InfoState.protectorName,
+});
+
+export const mapDispatchToProps = (dispatch:Function) => ({
+    setAddress: (
+        address: string,
+    ) => dispatch(setAddress(address)),
+    setBirthday: (
+        birthday: string,
+    ) => dispatch(setBirthday(birthday)),
+    setGender: (
+        gender: string,
+    ) => dispatch(setGender(gender)),
+    setName: (
+        name: string,
+    ) => dispatch(setName(name)),
+    setNumber: (
+        number: string,
+    ) => dispatch(setNumber(number)),
+    setPicture: (
+        picture: File,
+    ) => dispatch(setPicture(picture)),
+    setMiddleSchool: (
+        middleschool: string,
+    ) => dispatch(setMiddleSchool(middleschool)),
+    setSchoolPhoneNum: (
+        schoolPhoneNum: string,
+    ) => dispatch(setSchoolPhoneNum(schoolPhoneNum)),
+    setProtectorPhoneNum: (
+        protectorPhoneNum: string,
+    ) => dispatch(setProtectorPhoneNum(protectorPhoneNum)),
+    setPhoneNum: (
+        phoneNum: string,
+    ) => dispatch(setPhoneNum(phoneNum)),
+    setProtectorName: (
+        protectorName: string,
+    ) => dispatch(setProtectorName(protectorName)),
+})
+
+export default connect(
+    mapStateToProps,
+    mapDispatchToProps,
+)(Info);
