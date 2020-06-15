83cc136031de4ce2272b2b881cf6136a4be126f5
ohjunsang <pandati@dsm.hs.kr>
2020-06-15 19:06:23 +0900

    [CREATE] 깃 실수로 인한 파일 복구

---
+++ poodle/src/components/Grade/GradeInput/SchoolYear/column/SchoolYearColumn.tsx
@@ -0,0 +1,22 @@
+import React, { FC } from 'react';
+
+interface Props {
+    isHalf?: boolean,
+    children: React.ReactNode,
+}
+
+const SchoolYearColumn: FC<Props> = ({
+    children,
+    isHalf
+}) => {
+    return (
+        <td 
+            colSpan={1}
+            className={isHalf ? "header half_grade" : "grade header"}
+        >
+            {children}
+        </td>
+    )
+}
+
+export default SchoolYearColumn;
