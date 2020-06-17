4dc115fe8e4b71b80a18ca40446ec41dc92b83fe
ohjunsang <pandati@dsm.hs.kr>
2020-06-17 13:19:49 +0900

    [FIX] index files and 봉사시간 부분

--- poodle/src/components/Grade/VolanteerWorkTimeAttendance/Attendance/index.tsx
+++ poodle/src/components/Grade/VolanteerWorkTimeAttendance/Attendance/index.tsx
@@ -1,51 +1,87 @@
 import React, { FC } from 'react';
 import { AttendanceRowDiv } from '../../../../styles/Grade';
-import { 
-    PerceptionColumn,
-    LeaveLateColumn,
-    CutClassColumn,
-    AbsentColumn,
+import {
+  PerceptionColumn,
+  LeaveLateColumn,
+  CutClassColumn,
+  AbsentColumn,
 } from './column';
 
+type dispatchFuncType = (value: string) => void;
 
-const Attendance: FC = () => {
-    return (
-        <AttendanceRowDiv>
-            <tr>
-                <td 
-                    className="header bottom" 
-                    rowSpan={2}
-                >
-                    <div>
-                        출석 정보
-                    </div>
-                </td>
-                <td className="element">
-                    <div>
-                        <AbsentColumn/>
-                    </div>
-                </td>
-                <td className="element">
-                    <div>
-                        <LeaveLateColumn/>
-                    </div>
-                </td>
-            </tr>
-            <tr>
-                <td className="element">
-                    <div>
-                        <PerceptionColumn/>
-                    </div>
-                </td>
-                <td className="element">
-                    <div>
-                        <CutClassColumn/>
-                    </div>
-                </td>
-            </tr>
-        </AttendanceRowDiv>
-    )
+interface Props {
+    setAbsentDay: dispatchFuncType,
+    setCutClassDay: dispatchFuncType,
+    setLeaveLateDay: dispatchFuncType,
+    setPerceptionDay: dispatchFuncType,
+    absentDay: string,
+    leaveLateDay: string,
+    perceptionDay: string,
+    cutClassDay: string,
+    isError: boolean,
 }
 
+const Attendance: FC<Props> = ({
+  absentDay,
+  leaveLateDay,
+  perceptionDay,
+  cutClassDay,
+  setAbsentDay,
+  setCutClassDay,
+  setLeaveLateDay,
+  setPerceptionDay,
+  isError,
+}) => (
+  <AttendanceRowDiv>
+    <tr>
+      <td
+        className="header bottom"
+        rowSpan={2}
+      >
+        <div>
+          출석 정보
+        </div>
+      </td>
+      <td className="element">
+        <div>
+          <AbsentColumn
+            valueChangeHandler={setAbsentDay}
+            value={absentDay}
+            isError={isError}
+          />
+        </div>
+      </td>
+      <td className="element">
+        <div>
+          <LeaveLateColumn
+            valueChangeHandler={setLeaveLateDay}
+            value={leaveLateDay}
+            isError={isError}
+          />
+        </div>
+      </td>
+    </tr>
+    <tr>
+      <td className="element">
+        <div>
+          <PerceptionColumn
+            valueChangeHandler={setPerceptionDay}
+            value={perceptionDay}
+            isError={isError}
+          />
+        </div>
+      </td>
+      <td className="element">
+        <div>
+          <CutClassColumn
+            valueChangeHandler={setCutClassDay}
+            value={cutClassDay}
+            isError={isError}
+          />
+        </div>
+      </td>
+    </tr>
+  </AttendanceRowDiv>
+);
 
 export default Attendance;
