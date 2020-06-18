d9a4d1504b81d18c510a1fc28069402f4923fea1
_uchanlee <uchanlee.dev@gmail.com>
2020-06-18 19:28:10 +0900

    [UPDATE] dotenv

--- poodle/src/index.tsx
+++ poodle/src/index.tsx
@@ -4,7 +4,10 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { Provider } from 'react-redux';
 
+import dotenv from 'dotenv';
 import store from './core/redux/store';
+
+dotenv.config();
 
 ReactDOM.render(
   <Provider store={store}>
