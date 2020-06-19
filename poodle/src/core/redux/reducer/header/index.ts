250398e10e56c3f34af1de846fc290fe6d4ec42f
_uchanlee <uchanlee.dev@gmail.com>
2020-06-19 13:59:57 +0900

    [UPDATE] header reducer

--- poodle/src/core/redux/reducer/header/index.ts
+++ poodle/src/core/redux/reducer/header/index.ts
@@ -1,53 +1,74 @@
-import { 
-    LOGIN_SUCCESS,
-    LOGIN_FAILURE,
-    HeaderAction
+import {
+  LOGIN_SUCCESS,
+  LOGIN_FAILURE,
+  LOGIN_RESET,
+  HeaderAction
 } from '../../actions/header';
 
-
 type HeaderState = {
-    isLogin: boolean,
-    error: {
-        login: string
-    },
-    user: {
-        userName: string
-    },
-}
+  isLogin: boolean;
+  user: {
+    accessToken: string;
+    refreshToken: string;
+  };
+  error: {
+    response: {
+      code: number;
+    };
+  };
+};
 
 const initialState: HeaderState = {
-    isLogin: false,
-    error: {
-        login: ''
-    },
-    user: {
-        userName: ''
-    },
-}
-
-export default function header(state: HeaderState = initialState, action: HeaderAction) {
-    switch (action.type) {
-        case LOGIN_SUCCESS:
-            return {
-                ...state,
-                isLogin: true,
-                error: {
-                    login: ''
-                },
-                user: action.payload,
-            }
-        case LOGIN_FAILURE:
-            return {
-                ...state,
-                error: {
-                    ...state.error,
-                    login: action.payload
-                },
-                user: {
-                    userName: ''
-                }
-            }
-        default:
-            return state;
+  isLogin: false,
+  user: {
+    accessToken: '',
+    refreshToken: ''
+  },
+  error: {
+    response: {
+      code: 0
     }
+  }
+};
+
+export default function header(
+  state: HeaderState = initialState,
+  action: HeaderAction
+) {
+  switch (action.type) {
+    case LOGIN_SUCCESS:
+      return {
+        ...state,
+        isLogin: true,
+        user: action.payload
+      };
+    case LOGIN_FAILURE:
+      return {
+        ...state,
+        user: {
+          accessToken: '',
+          refreshToken: ''
+        },
+        error: {
+          ...action.payload,
+          response: ''
+        }
+      };
+    case LOGIN_RESET:
+      return {
+        ...state,
+        isLogin: false,
+        user: {
+          accessToken: '',
+          refreshToken: ''
+        },
+        error: {
+          response: {
+            code: 0
+          }
+        }
+      };
+    default:
+      return state;
+  }
 }
