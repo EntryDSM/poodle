514b29767b876fe28610318fa8b5bf36caa88dac
ohjunsang <pandati@dsm.hs.kr>
2020-07-04 13:44:36 +0900

    [FIX] export reducer type

--- poodle/src/core/redux/store/index.ts
+++ poodle/src/core/redux/store/index.ts
@@ -1,15 +1,19 @@
 import { createStore, applyMiddleware } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import createSagaMiddleware from 'redux-saga';
+import { createLogger } from 'redux-logger';
 
 import rootSaga from '../sagas';
 import rootReducer from '../reducer';
 
 const sagaMiddleware = createSagaMiddleware();
+const logger = createLogger();
 const store = createStore(
-    rootReducer,
-    composeWithDevTools(applyMiddleware(sagaMiddleware))
+  rootReducer,
+  composeWithDevTools(applyMiddleware(sagaMiddleware)),
 );
 
 sagaMiddleware.run(rootSaga);
+
+export type ReducerType = ReturnType<typeof rootReducer>;
 export default store;
