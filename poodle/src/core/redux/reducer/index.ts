2c8feafc4384a731caa0c6a2e61f12177b5d1292
_uchanlee <uchanlee.dev@gmail.com>
2020-07-09 19:31:55 +0900

    [UPDATE] <reducer> index - about join

--- poodle/src/core/redux/reducer/index.ts
+++ poodle/src/core/redux/reducer/index.ts
@@ -2,11 +2,13 @@ import { combineReducers } from 'redux';
 import header from './header';
 import loading from './loading';
 import modal from './modal';
+import join from './join';
 
 const rootReducer = combineReducers({
-    header,
-    loading,
-    modal,
+  header,
+  loading,
+  modal,
+  join,
 });
 export type RootState = ReturnType<typeof rootReducer>;
 export default rootReducer;
