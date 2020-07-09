2094f364631565929f32a7e63d53e30a580ddb10
ohjunsang <pandati@dsm.hs.kr>
2020-07-09 14:43:08 +0900

    [FIX] prettier 적용 중요도 없음

--- poodle/src/lib/api/ServerUrl/index.ts
+++ poodle/src/lib/api/ServerUrl/index.ts
@@ -1,6 +1,6 @@
-export const USERTYPE_URL = "/users/me/grade-type";
-export const GRADE_URL = "/application/grade";
-export const USERINFO_URL = "/users";
-export const SELF_INTRODUCTION_URL = "/applications/me/grade";
-export const STUDY_PLAN_URL = "/applications/me/plan";
-export const KAKAO_ADDRESS_URL = "https://dapi.kakao.com/v2/local/search/address.json";
+export const USERTYPE_URL = '/users/me/grade-type';
+export const GRADE_URL = '/application/grade';
+export const USERINFO_URL = '/users';
+export const SELF_INTRODUCTION_URL = '/applications/me/grade';
+export const STUDY_PLAN_URL = '/applications/me/plan';
+export const KAKAO_ADDRESS_URL = 'https://dapi.kakao.com/v2/local/search/address.json';
--- poodle/src/lib/api/kakao/index.ts
+++ poodle/src/lib/api/kakao/index.ts
@@ -1,19 +1,19 @@
 import axios from 'axios';
 import { KAKAO_ADDRESS_URL } from '../ServerUrl';
 import { kakaoSearchedAddressType } from '../ApiType';
+
 const getSearchedAddressApi = async (searchParams: string) => {
-    const header = {
-        headers: {
-            Authorization: `KakaoAK 2c8f36e27e83eb36b73d5d4aea7a7068`,
-        },
-        params: {
-            query: searchParams,
-        }
-    }
-    const response = await axios.get(KAKAO_ADDRESS_URL, header);
-    console.log(response)
-    const data:kakaoSearchedAddressType[] = response.data.documents;
-    return data;
-}
+  const header = {
+    headers: {
+      Authorization: 'KakaoAK 2c8f36e27e83eb36b73d5d4aea7a7068',
+    },
+    params: {
+      query: searchParams,
+    },
+  };
+  const response = await axios.get(KAKAO_ADDRESS_URL, header);
+  const data:kakaoSearchedAddressType[] = response.data.documents;
+  return data;
+};
 
 export default getSearchedAddressApi;
