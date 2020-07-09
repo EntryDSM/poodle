0a5d62178dd4753e37f4ef3c650056dacc593ae7
_uchanlee <uchanlee.dev@gmail.com>
2020-07-09 19:34:05 +0900

    [UPDATE] <utils> createRequestSaga - add error type

--- poodle/src/lib/utils/saga/createRequestSaga.ts
+++ poodle/src/lib/utils/saga/createRequestSaga.ts
@@ -1,5 +1,6 @@
 import { call, put } from 'redux-saga/effects';
 import { startLoading, finishLoading } from '@/core/redux/actions/loading';
+import ErrorType from '../type/ErrorType';
 
 export default function createRequestSaga(type: any, request: any) {
     const SUCCESS = `${type}_SUCCESS`;
@@ -13,9 +14,10 @@ export default function createRequestSaga(type: any, request: any) {
                 payload: response.data
             });
         } catch (e) {
+            const error: ErrorType = e;
             yield put({
                 type: FAILURE,
-                payload: e,
+                payload: error,
                 error: true
             });
         }
