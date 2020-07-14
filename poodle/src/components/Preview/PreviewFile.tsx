d8afbcff507670f127f32d46fc630b0b23558dd9
ohjunsang <pandati@dsm.hs.kr>
2020-07-14 19:01:38 +0900

    [FIX] prettier로 인한 단순한 파일 구조 변화

--- poodle/src/components/Preview/PreviewFile.tsx
+++ poodle/src/components/Preview/PreviewFile.tsx
@@ -1,26 +1,14 @@
-import React, {
-    FC,
-} from 'react';
-import { 
-    PreviewPdfDiv,
-} from '../../styles/Preview';
+import React, { FC } from 'react';
+import { PreviewPdfDiv } from '../../styles/Preview';
 
 interface Props {
-    src: string
+  src: string;
 }
 
-const PreviewFile: FC<Props> = ({
-    src,
-}) => {
-    return (
-        <PreviewPdfDiv>
-            <iframe
-                src={src}
-                width="1170px" 
-                height="1072px"
-            />
-        </PreviewPdfDiv>
-    )
-}
+const PreviewFile: FC<Props> = ({ src }) => (
+  <PreviewPdfDiv>
+    <embed src={src} width='1170px' height='1072px' />
+  </PreviewPdfDiv>
+);
 
 export default PreviewFile;
