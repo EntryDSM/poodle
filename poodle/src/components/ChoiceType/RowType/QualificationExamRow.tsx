ece834c50987502a776448f343a6aef7009988d4
ohjunsang <pandati@dsm.hs.kr>
2020-07-14 13:10:11 +0900

    [FIX] prettier로 인한 단순한 형식 변화 - choiceType/RowType

--- poodle/src/components/ChoiceType/RowType/QualificationExamRow.tsx
+++ poodle/src/components/ChoiceType/RowType/QualificationExamRow.tsx
@@ -1,34 +1,39 @@
-import React, { FC } from 'react';
+import React, { FC, useCallback } from 'react';
 import { TypeElementContent } from '../../../styles/ChoiceType';
 import { Checkbox } from '../../default/ApplicationFormDefault';
 import { DefaultRow } from '..';
 
 interface Props {
-    description?: string,
-    valueChangeHandler: (isQualificationExam:boolean) => void,
-    qualificationExam: boolean,
+  description?: string;
+  valueChangeHandler: (isQualificationExam: boolean) => void;
+  qualificationExam: boolean;
 }
 
-const QualificationExam: FC<Props> = ({ 
-    description, 
-    valueChangeHandler,
-    qualificationExam,
+const QualificationExam: FC<Props> = ({
+  description,
+  valueChangeHandler,
+  qualificationExam,
 }) => {
-    return (
-        <DefaultRow title="검정고시 지원 여부">
-            <TypeElementContent>
-                <div>
-                    <Checkbox 
-                        checked={qualificationExam}
-                        onChange={valueChangeHandler}
-                    />
-                </div>
-                <div>
-                    <p>{description}</p>
-                </div>
-            </TypeElementContent>
-        </DefaultRow>
-    )
-}
+  const checkboxChangeHandler = useCallback((isQualificationExam: boolean) => {
+    const valueToString = isQualificationExam ? 'true' : 'false';
+    window.localStorage.setItem('isQualificationExam', valueToString);
+    valueChangeHandler(isQualificationExam);
+  }, []);
+  return (
+    <DefaultRow title='검정고시 지원 여부'>
+      <TypeElementContent>
+        <div>
+          <Checkbox
+            checked={qualificationExam}
+            onChange={checkboxChangeHandler}
+          />
+        </div>
+        <div>
+          <p>{description}</p>
+        </div>
+      </TypeElementContent>
+    </DefaultRow>
+  );
+};
 
 export default QualificationExam;
