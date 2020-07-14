a7a958ddb681f25bafb8919362c32028846bf1a7
ohjunsang <pandati@dsm.hs.kr>
2020-07-14 18:59:21 +0900

    [FIX] assets fix with prettier

--- poodle/src/assets/Join/index.ts
+++ poodle/src/assets/Join/index.ts
@@ -1,5 +1,5 @@
 export { default as check } from './check.png';
 export { default as checkedImage } from './checked.png';
 export { default as rightArrow } from './rightArrow.png';
-export { default as validRightArrow } from './validRightArrow.png'
+export { default as validRightArrow } from './validRightArrow.png';
 export { default as inputCheck } from './inputCheck.png';
