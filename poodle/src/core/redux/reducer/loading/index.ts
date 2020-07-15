fc12fd8d4a32464f91018b28cfabfda9b064452d
_uchanlee <uchanlee.dev@gmail.com>
2020-07-15 12:15:08 +0900

    [UPDATE] <reducer> loading - about join

--- poodle/src/core/redux/reducer/loading/index.ts
+++ poodle/src/core/redux/reducer/loading/index.ts
@@ -4,13 +4,20 @@ import {
   LoadingAction,
 } from '../../actions/loading';
 import { LOGIN } from '../../actions/header';
+import { SEND_EMAIL, VERIFY_CODE, JOIN } from '../../actions/join';
 
 type LoadingState = {
   [LOGIN]: boolean;
+  [SEND_EMAIL]: boolean;
+  [VERIFY_CODE]: boolean;
+  [JOIN]: boolean;
 };
 
 const initialState: LoadingState = {
   [LOGIN]: false,
+  [SEND_EMAIL]: false,
+  [VERIFY_CODE]: false,
+  [JOIN]: false,
 };
 
 export default function loading(
