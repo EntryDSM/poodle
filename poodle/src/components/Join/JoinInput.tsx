5edcbbbee5e85352b8bd0f96a51045ef531f9456
_uchanlee <uchanlee.dev@gmail.com>
2020-07-15 12:44:20 +0900

    [UPDATE] <JoinInput> input logic

--- poodle/src/components/Join/JoinInput.tsx
+++ poodle/src/components/Join/JoinInput.tsx
@@ -1,11 +1,32 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 import * as S from '../../styles/Join';
+import { InputAction } from './inputReducer';
 
 type JoinInputProps = {
   maxSize?: boolean;
   middleSize?: boolean;
+  isDisabled?: boolean;
+  label: 'email' | 'code' | 'password' | 'passwordCheck';
+  value: {
+    email: string;
+    code: string;
+    password: string;
+    passwordCheck: string;
+  };
+  setValue: Function;
+  isFocused: {
+    email: boolean;
+    code: boolean;
+    password: boolean;
+    passwordCheck: boolean;
+  };
+  inputDispatch: React.Dispatch<InputAction>;
+  inputFocused?: {
+    password: boolean;
+    passwordCheck: boolean;
+  };
+  setInputFocused?: Function;
 };
-
 
 function JoinInput({
   maxSize,
@@ -17,28 +38,29 @@ function JoinInput({
   isFocused,
   inputDispatch,
   inputFocused,
-  setInputFocused
+  setInputFocused,
 }: JoinInputProps) {
   const isPasswordType = label === 'password' || label === 'passwordCheck';
   const onChange = useCallback(
     (e: React.ChangeEvent<HTMLInputElement>) => {
+      console.log(isFocused[label]);
       if (!isFocused[label]) return;
       inputDispatch(setValue(e.target.value));
     },
-    [isFocused]
+    [isFocused],
   );
   const onFocus = useCallback(() => {
     if (setInputFocused)
       setInputFocused({
         ...inputFocused,
-        [label]: true
+        [label]: true,
       });
   }, [inputFocused]);
   const onBlur = useCallback(() => {
     if (setInputFocused)
       setInputFocused({
         ...inputFocused,
-        [label]: false
+        [label]: false,
       });
   }, [inputFocused]);
   return (
