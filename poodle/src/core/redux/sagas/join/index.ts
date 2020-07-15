3143dee03e325e56447d8e693bb2a697dcdfaf19
_uchanlee <uchanlee.dev@gmail.com>
2020-07-15 12:16:42 +0900

    [CREATE] <saga> join

---
+++ poodle/src/core/redux/sagas/join/index.ts
@@ -0,0 +1,14 @@
+import { SEND_EMAIL, VERIFY_CODE, JOIN } from '../../actions/join';
+import { takeLatest } from 'redux-saga/effects';
+import createRequestSaga from '@/lib/utils/saga/createRequestSaga';
+import * as joinCtrl from '@/lib/api/join';
+
+const emailSendSaga = createRequestSaga(SEND_EMAIL, joinCtrl.emailSend);
+const verifyCodeSaga = createRequestSaga(VERIFY_CODE, joinCtrl.verifyCode);
+const joinSaga = createRequestSaga(JOIN, joinCtrl.join);
+
+export default function* headerSaga() {
+  yield takeLatest(SEND_EMAIL, emailSendSaga);
+  yield takeLatest(VERIFY_CODE, verifyCodeSaga);
+  yield takeLatest(JOIN, joinSaga);
+}
