54f394b43f8bb22b706439481b52dd99c4118e95
ohjunsang <pandati@dsm.hs.kr>
2020-07-18 21:59:58 +0900

    [FIX] index.ts

--- poodle/src/lib/utils/saga/index.ts
+++ poodle/src/lib/utils/saga/index.ts
@@ -1,2 +1,3 @@
 export { default as createSaveSaga } from './createSaveSaga';
 export { default as createProxySaga } from './createProxySaga';
+export { default as createGetSaga } from './createGetSaga';
