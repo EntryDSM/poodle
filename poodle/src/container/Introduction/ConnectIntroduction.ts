aca925522bc8d5c023631d569c525b9ae9b95eec
ohjunsang <pandati@dsm.hs.kr>
2020-07-26 21:48:34 +0900

    [FIX] - introduction 서버통신 모두 saga로 수정

--- poodle/src/container/Introduction/ConnectIntroduction.ts
+++ poodle/src/container/Introduction/ConnectIntroduction.ts
@@ -4,17 +4,27 @@ import { RootState } from '@/core/redux/reducer';
 import {
   setSelfIntoduction,
   setStudyPlan,
+  getStudyPlanCall,
+  studyPlanCall,
+  getSelfIntroductionCall,
+  selfIntroductionCall,
 } from '@/core/redux/actions/Introduction';
 
 export const mapStateToProps = (state: RootState) => ({
   selfIntroduction: state.IntroductionState.selfIntroduction,
   studyPlan: state.IntroductionState.studyPlan,
+  error: state.IntroductionState.error,
+  page: state.PageState.page,
 });
 
 export const mapDispatchToProps = (dispatch: Function) => ({
   setSelfIntroduction: (selfIntroduction: string) =>
     dispatch(setSelfIntoduction({ selfIntroduction })),
   setStudyPlan: (studyPlan: string) => dispatch(setStudyPlan({ studyPlan })),
+  getStudyPlanToServer: () => dispatch(getStudyPlanCall()),
+  setStudyPlanToServer: () => dispatch(studyPlanCall()),
+  setSelfIntroductionToServer: () => dispatch(selfIntroductionCall()),
+  getSelfIntroductionToServer: () => dispatch(getSelfIntroductionCall()),
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(Introduction);
