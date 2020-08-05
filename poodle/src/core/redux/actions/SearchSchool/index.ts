381cf7f15e396e57b7e306e72b3c34db9cf2506f
ohjunsang <pandati@dsm.hs.kr>
2020-08-05 13:20:42 +0900

    feat: SearchSchool action

---
+++ poodle/src/core/redux/actions/SearchSchool/index.ts
@@ -0,0 +1,113 @@
+import ErrorType from '@/lib/utils/type/ErrorType';
+
+export const GET_SCHOOL_CALL = 'SearchSchool/GET_SCHOOL' as const;
+export const GET_SCHOOL_FAILURE = 'SearchSchool/GET_SCHOOL_FAILURE' as const;
+export const GET_SCHOOL_SUCCESS = 'SearchSchool/GET_SCHOOL_SUCCESS' as const;
+export const SCHOOL_INFO = 'SearchSchool/SCHOOL_INFO' as const;
+export const EDU_OFFICE = 'SearchSchool/EDU_OFFICE' as const;
+export const SCHOOL_SEARCH_INPUT = 'SearchScool/SCHOOL_SEARCH_INPUT' as const;
+export const PAGE = 'SearchScool/PAGE' as const;
+export const LOADING = 'SearchSchool/LOADING' as const;
+
+export type SchoolType = {
+  schoolCode: string;
+  schoolName: string;
+  schoolFullName: string;
+  schoolAddress: string;
+};
+
+export interface GetSchoolCall {
+  type: typeof GET_SCHOOL_CALL;
+  payload: { url: string };
+}
+
+export interface GetSchoolFailure {
+  type: typeof GET_SCHOOL_FAILURE;
+  payload: { error: ErrorType };
+}
+
+export interface GetSchoolSuccess {
+  type: typeof GET_SCHOOL_SUCCESS;
+  payload: { info: SchoolType[] };
+}
+
+export interface SchoolInfo {
+  type: typeof SCHOOL_INFO;
+  payload: { info: SchoolType[] };
+}
+
+export interface EduOffice {
+  type: typeof EDU_OFFICE;
+  payload: { office: string };
+}
+
+export interface SchoolSearchInput {
+  type: typeof SCHOOL_SEARCH_INPUT;
+  payload: { searchInput: string };
+}
+
+export interface Page {
+  type: typeof PAGE;
+  payload: { page: number };
+}
+
+export interface Loading {
+  type: typeof LOADING;
+  payload: { loading: boolean };
+}
+
+export type SearchSchoolType =
+  | GetSchoolCall
+  | GetSchoolFailure
+  | GetSchoolSuccess
+  | SchoolInfo
+  | EduOffice
+  | SchoolSearchInput
+  | Page
+  | Loading;
+
+export const getSchoolCall = (payload: { url: string }): SearchSchoolType => ({
+  type: GET_SCHOOL_CALL,
+  payload,
+});
+
+export const getSchoolFailure = (payload: {
+  error: ErrorType;
+}): SearchSchoolType => ({
+  type: GET_SCHOOL_FAILURE,
+  payload,
+});
+
+export const getSchoolSuccess = (payload: {
+  info: SchoolType[];
+}): SearchSchoolType => ({
+  type: GET_SCHOOL_SUCCESS,
+  payload,
+});
+
+export const schoolInfoChange = (payload: {
+  info: SchoolType[];
+}): SearchSchoolType => ({
+  type: SCHOOL_INFO,
+  payload,
+});
+
+export const eduOffice = (payload: { office: string }) => ({
+  type: EDU_OFFICE,
+  payload,
+});
+
+export const schoolSearchInput = (payload: { searchInput: string }) => ({
+  type: SCHOOL_SEARCH_INPUT,
+  payload,
+});
+
+export const page = (payload: { page: number }) => ({
+  type: PAGE,
+  payload,
+});
+
+export const loading = (payload: { loading: boolean }) => ({
+  type: LOADING,
+  payload,
+});
