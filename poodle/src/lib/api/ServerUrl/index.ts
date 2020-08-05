4812af7b6f73ee8c7120500e5ff8836784b3c2bf
ohjunsang <pandati@dsm.hs.kr>
2020-08-05 12:51:58 +0900

    feat: add searchSchool url

--- poodle/src/lib/api/ServerUrl/index.ts
+++ poodle/src/lib/api/ServerUrl/index.ts
@@ -2,3 +2,4 @@ export const USERTYPE_URL = '/users/me/grade-type';
 export const GRADE_URL = '/application/grade';
 export const USERINFO_URL = '/users';
 export const INTRODUCTION_URL = '/applications/me/plan';
+export const GET_SCHOOL_URL = '/schools';
