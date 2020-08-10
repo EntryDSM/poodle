8b795bd5fd2dd4e2123f0c8a62c9533be2085e83
_uchanlee <uchanlee.dev@gmail.com>
2020-08-10 15:07:34 +0900

    [CREATE] <components/~/ResetModal/index.ts> export
    components

---
+++ poodle/src/components/default/common/Modal/ResetModal/index.ts
@@ -0,0 +1,5 @@
+export { default as EmailPage } from './EmailPage';
+export { default as PasswordCheckPage } from './PasswordCheckPage';
+export { default as PasswordPage } from './PasswordPage';
+export { default as SuccessPage } from './SuccessPage';
+export { default as VerifyCodePage } from './VerifyCodePage';
