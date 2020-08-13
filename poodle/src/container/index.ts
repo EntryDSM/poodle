597f3fd353c1afd09370790108517c455fc476fd
ohjunsang <pandati@dsm.hs.kr>
2020-08-13 17:19:08 +0900

    [FIX] naming 수정

--- poodle/src/container/index.ts
+++ poodle/src/container/index.ts
@@ -4,5 +4,5 @@ export { default as JoinContainer } from './JoinContainer/JoinContainer';
 export { Introduction, ConnectIntroduction } from './Introduction';
 export { ChoiceType, ConnectChoiceType } from './ChoiceType';
 export { ConnectInfo } from './Info';
-export { Preview } from './Preview';
+export { PreviewContainer } from './Preview';
 export { ConnectGrade } from './Grade';
