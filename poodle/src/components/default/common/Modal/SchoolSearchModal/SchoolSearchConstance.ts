4f1bf05678eb97dadf85a128651585e9bcbc3016
ohjunsang <pandati@dsm.hs.kr>
2020-08-22 22:11:04 +0900

    [ADD] constance 추가

--- poodle/src/components/default/common/Modal/SchoolSearchModal/SchoolSearchConstance.ts
+++ poodle/src/components/default/common/Modal/SchoolSearchModal/SchoolSearchConstance.ts
@@ -4,11 +4,12 @@ interface OptionType {
 }
 
 const eduOfficeList: OptionType[] = [
-  { LABEL: '대전광역시 교육청', VALUE: 'sth' },
-  { LABEL: '경상북도 교육청', VALUE: 'sth1' },
-  { LABEL: '경기도 교육청', VALUE: 'sth2' },
-  { LABEL: '경상남도 교육청', VALUE: 'sth3' },
-  { LABEL: '충청북도 교육청', VALUE: 'sth4' },
+  { LABEL: '대전 교육청', VALUE: '대전' },
+  { LABEL: '경상북도 교육청', VALUE: '경상북도' },
+  { LABEL: '경기도 교육청', VALUE: '경기도' },
+  { LABEL: '경상남도 교육청', VALUE: '경상남도' },
+  { LABEL: '충청북도 교육청', VALUE: '충청북도' },
+  { LABEL: '서울 교육청', VALUE: '서울' },
 ];
 
 export default eduOfficeList;
