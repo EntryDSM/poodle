2718de1d853d4fc51dbf532415dc14239e5efb34
ohjunsang <pandati@dsm.hs.kr>
2020-08-27 23:32:37 +0900

    [ADD] choiceType에서 notice modal 띄우게 수정

--- poodle/src/container/ChoiceType/ConnectChoiceType.ts
+++ poodle/src/container/ChoiceType/ConnectChoiceType.ts
@@ -17,8 +17,8 @@ import {
   getTypeCall,
   typeCall,
 } from '@/core/redux/actions/ChoiceType';
+import { NOTICE_MODAL, modalOn } from '@/core/redux/actions/Modal';
 import { GraduationStatusType } from '@/core/redux/actions/ChoiceType';
-
 export const mapStateToProps = (state: RootState) => ({
   qualificationExam: state.QualificationState.isQualification,
   applyType: state.ChoiceTypeState.applyType,
@@ -51,6 +51,7 @@ export const mapDispatchToProps = (dispatch: Function) => ({
   setGEDSuccessMonth: (month: string) =>
     dispatch(setGEDSuccessMonth({ month })),
   setGEDSuccessYear: (year: string) => dispatch(setGEDSuccessYear({ year })),
+  modalOn: () => dispatch(modalOn(NOTICE_MODAL)),
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(ChoiceType);
