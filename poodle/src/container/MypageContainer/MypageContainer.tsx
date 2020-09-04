1143f673910aacad61e3d5383f15b5284c691665
_uchanlee <uchanlee.dev@gmail.com>
2020-09-04 18:33:57 +0900

    [UPDATE] <@/container/~/MypageContainer.tsx> connecct redux

--- poodle/src/container/MypageContainer/MypageContainer.tsx
+++ poodle/src/container/MypageContainer/MypageContainer.tsx
@@ -2,23 +2,42 @@ import React, { FC } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { RootState } from '@/core/redux/reducer';
 import { Mypage } from '@/components';
-import { process as createProcessAction } from '@/core/redux/actions/Mypage';
+import {
+  process as createProcessAction,
+  userStatus as createUserStatusAction,
+} from '@/core/redux/actions/Mypage';
 
 const MypageContainer: FC = () => {
   const dispatch = useDispatch();
-  const { process } = useSelector(
+  const { process, userStatus, userStatusError } = useSelector(
     ({ Mypage: mypage, Loading: loading }: RootState) => ({
       process: {
         data: mypage.process,
         error: mypage.processError,
         loading: loading['mypage/PROCESS'],
       },
+      userStatus: mypage.userStatus,
+      userStatusError: mypage.userStatueError,
     }),
   );
+
   const getProcess = () => {
     dispatch(createProcessAction());
   };
-  return <Mypage process={process} getProcess={getProcess} />;
+
+  const getUserStatus = () => {
+    dispatch(createUserStatusAction());
+  };
+
+  return (
+    <Mypage
+      process={process}
+      getProcess={getProcess}
+      getUserStatus={getUserStatus}
+      userStatus={userStatus}
+      userStatusError={userStatusError}
+    />
+  );
 };
 
 export default MypageContainer;
