6ef62838e4bb5a4b92b72cecf22da557bfe8be01
_uchanlee <uchanlee.dev@gmail.com>
2020-09-04 18:33:43 +0900

    [ADD] <@/core/~/actions/Mypage/index.ts> action types

--- poodle/src/core/redux/actions/Mypage/index.ts
+++ poodle/src/core/redux/actions/Mypage/index.ts
@@ -1,8 +1,13 @@
 import ErrorType from '@/lib/utils/type';
+import { UserStatus } from '@/lib/api/mypage';
 
 export const PROCESS = 'mypage/PROCESS' as const;
 export const PROCESS_SUCCESS = 'mypage/PROCESS_SUCCESS' as const;
 export const PROCESS_FAILURE = 'mypage/PROCESS_FAILURE' as const;
+
+export const USER_STATUS = 'mypage/USER_STATUS' as const;
+export const USER_STATUS_SUCCESS = 'mypage/USER_STATUS_SUCCESS' as const;
+export const USER_STATUS_FAILURE = 'mypage/USER_STATUS_FAILURE' as const;
 
 export const process = () => ({
   type: PROCESS,
@@ -25,7 +30,23 @@ export const processFailure = (e: ErrorType) => ({
   payload: e,
 });
 
+export const userStatus = () => ({
+  type: USER_STATUS,
+});
+
+export const userStatusSuccess = (userStatus: UserStatus) => ({
+  type: USER_STATUS_SUCCESS,
+  payload: userStatus,
+});
+
+export const userStatusFailure = (error: ErrorType) => ({
+  type: USER_STATUS_FAILURE,
+  payload: error,
+});
+
 export type MypageAction =
   | ReturnType<typeof process>
   | ReturnType<typeof processSuccess>
-  | ReturnType<typeof processFailure>;
+  | ReturnType<typeof processFailure>
+  | ReturnType<typeof userStatusSuccess>
+  | ReturnType<typeof userStatusFailure>;
