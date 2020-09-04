da0480ec65390b26fdbce3aa6d3554b23df95b41
_uchanlee <uchanlee.dev@gmail.com>
2020-09-04 18:31:48 +0900

    [UPDATE] <@/lib/api/mypage.ts> axios generic type and add api

--- poodle/src/lib/api/mypage.ts
+++ poodle/src/lib/api/mypage.ts
@@ -1,8 +1,15 @@
-import client from './client';
+import { getClientWithAccessToken } from './client';
 
-export const getProcess = () =>
-  client.get('/process/me', {
-    headers: {
-      Authorization: localStorage.getItem('accessToken'),
-    },
-  });
+export const getProcess = () => getClientWithAccessToken().get('/process/me');
+
+export interface UserStatus {
+  name: string;
+  sex: 'MALE' | 'FEMALE';
+  paid: boolean;
+  printed_application_arrived: boolean;
+  passed_first_apply: boolean;
+  passed_interview: boolean;
+  final_submit: boolean;
+}
+export const getUserStatus = () =>
+  getClientWithAccessToken().get<UserStatus>('/users/me/status');
