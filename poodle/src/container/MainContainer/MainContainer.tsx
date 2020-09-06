bd5002a16dfba7cef07f1e83d64786b7a7d7e0ae
_uchanlee <uchanlee.dev@gmail.com>
2020-09-06 14:08:32 +0900

    [UPDATE] <@/container/~/MainContainer.tsx> connect redux

--- poodle/src/container/MainContainer/MainContainer.tsx
+++ poodle/src/container/MainContainer/MainContainer.tsx
@@ -1,6 +1,27 @@
 import React, { FC } from 'react';
 import Main from '@/components/Main/Main';
+import { useSelector, useDispatch } from 'react-redux';
+import { RootState } from '@/core/redux/reducer';
+import { schedules as createSchedulesAction } from '@/core/redux/actions/Main';
 
-const MainContainer: React.FC<{}> = () => <Main />;
+const MainContainer: FC = () => {
+  const dispatch = useDispatch();
+  const { schedules, isLoading } = useSelector(
+    ({ Main: main, Loading: loading }: RootState) => ({
+      schedules: main.schedules,
+      isLoading: loading['main/SCHEDULES'],
+    }),
+  );
+  const getScheduls = () => {
+    dispatch(createSchedulesAction());
+  };
+  return (
+    <Main
+      schedules={schedules}
+      isLoading={isLoading}
+      getScheduls={getScheduls}
+    />
+  );
+};
 
 export default MainContainer;
