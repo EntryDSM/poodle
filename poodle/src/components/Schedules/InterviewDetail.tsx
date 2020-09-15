a8e4e94bd063c2e1c3ac0335964d5e5ceef1ebee
_uchanlee <uchanlee.dev@gmail.com>
2020-09-15 16:48:06 +0900

    [UPDATE] <components/~/InterviewDetail.tsx> token expired handling

--- poodle/src/components/Schedules/InterviewDetail.tsx
+++ poodle/src/components/Schedules/InterviewDetail.tsx
@@ -2,7 +2,11 @@ import React, { FC, useEffect } from 'react';
 import ScheduleDetail from './ScheduleDetail';
 import { Schedule } from '@/core/redux/actions/Main';
 import ErrorType from '@/lib/utils/type';
-import { isProgressingSchedule, getFullDateText } from '@/lib/utils/function';
+import {
+  isProgressingSchedule,
+  getFullDateText,
+  useReGenerateTokenAndDoCallback,
+} from '@/lib/utils/function';
 import {
   INTERVIEW_INFO,
   INTERVIEWING_INFO,
@@ -27,6 +31,9 @@ const InterviewDetail: FC<Props> = ({
   getUserStatus,
   isLoading,
 }) => {
+  const reGenerateTokenAndGetUserStatus = useReGenerateTokenAndDoCallback(
+    getUserStatus,
+  );
   const isProgressing = isProgressingSchedule(schedules[INTERVIEW_INDEX]);
   const interview = INTERVIEW_INFO(
     getFullDateText(schedules[INTERVIEW_INDEX].start_date),
@@ -41,7 +48,9 @@ const InterviewDetail: FC<Props> = ({
   }, []);
 
   useEffect(() => {
-    if (userStatusError.status) {
+    if (userStatusError.status === 401) {
+      reGenerateTokenAndGetUserStatus();
+    } else if (userStatusError.status) {
       alert(`Error code: ${userStatusError.status} 상태 불러오기 실패!`);
     }
   }, [userStatusError]);
