63a1656793038f7ddb2ed6d6725ee0226f0fda1a
_uchanlee <uchanlee.dev@gmail.com>
2020-10-05 13:44:43 +0900

    [UPDATE] <@/lib/RegExp/index.ts> emailRegExp

--- poodle/src/lib/RegExp/index.ts
+++ poodle/src/lib/RegExp/index.ts
@@ -1,6 +1,4 @@
-export const emailRegExp = new RegExp(
-  '^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*.[a-zA-Z]{2,3}$',
-);
+export const emailRegExp = /^[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*\.[a-zA-Z]{2,3}$/i;
 
 export const passwordRegExp = new RegExp(
   '^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,}$',
