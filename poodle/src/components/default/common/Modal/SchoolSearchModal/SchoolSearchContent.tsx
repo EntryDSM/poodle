b9c0844313bb97be4b9d6e6848fe895ad3893dcd
ohjunsang <pandati@dsm.hs.kr>
2020-10-06 13:15:18 +0900

    [FIX] school search content 에 full_name 추가

--- poodle/src/components/default/common/Modal/SchoolSearchModal/SchoolSearchContent.tsx
+++ poodle/src/components/default/common/Modal/SchoolSearchModal/SchoolSearchContent.tsx
@@ -3,18 +3,21 @@ import {
   SearchModalContentText,
   SearchModalContent,
   SearchModalCategory,
+  SearchModalEmptyCategory,
 } from '@/styles/common/Modal';
 
 interface Props {
   schoolName: string;
   address: string;
   schoolCode: string;
+  schoolFullName: string;
   onClick: (schoolName: string, code: string) => void;
 }
 
 const SchoolSearchContent: FC<Props> = ({
   schoolName,
   address,
+  schoolFullName,
   onClick,
   schoolCode,
 }) => {
@@ -26,12 +29,14 @@ const SchoolSearchContent: FC<Props> = ({
       <div onClick={clickContentHandler}>
         <div>
           <SearchModalCategory>학교명</SearchModalCategory>
+          <SearchModalEmptyCategory></SearchModalEmptyCategory>
           <SearchModalCategory>주소</SearchModalCategory>
         </div>
         <div>
           <SearchModalContentText className='school'>
             {schoolName}
           </SearchModalContentText>
+          <SearchModalContentText>{schoolFullName}</SearchModalContentText>
           <SearchModalContentText>{address}</SearchModalContentText>
         </div>
       </div>
